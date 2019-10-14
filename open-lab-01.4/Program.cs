499718ad374e8337e6d242747256170cf02a16ed
martinspilak <martinecko24@gmail.com>
2019-10-14 09:01:12 +0200

    open-lab-01.4

---
+++ open-lab-01.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace open_lab_01._4
+{
+    class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return (a * b) / 2;
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum(a, b));
+        }
+    }
+}
